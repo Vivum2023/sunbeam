9325fc68f832d9e0e3cbe7128b8877f63b543436
TheRealToxicDev <toxic.dev09@gmail.com>
2023-05-03 13:40:02 +0000

    add reqmodal

--- cogs/request_roles.py
+++ cogs/request_roles.py
@@ -1,7 +1,48 @@
+import logging
 from discord.ext import commands
 import discord
 from bot import Vivum
 from config import roles
+
+class RequestModal(discord.ui.Modal, title='Identity'):
+    name = discord.ui.TextInput(label='Your Full Name')
+
+    def __init__(self, bot: Vivum, user_id: int, dept: str, is_hod: bool):
+        self.bot: Vivum = bot
+        self.user_id: int = user_id
+        self.dept: str = dept
+        self.is_hod: bool = is_hod
+        super().__init__(timeout=None)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        if interaction.user.id != self.user_id:
+            return await interaction.response.send_message("You cannot accept this request", ephemeral=True)
+    
+        # Send message to chat channel
+        admin_chat = discord.utils.get(self.bot.get_all_channels(), name="admin-chat")
+
+        if not admin_chat:
+            logging.error("Backup logs channel not found")
+            return await interaction.response.send_message("Internal error: logs channel not found", ephemeral=True)
+        
+        if not isinstance(admin_chat, discord.TextChannel):
+            logging.error("Backup logs channel not a text channel")
+            return await interaction.response.send_message("Internal error: logs channel not a text channel", ephemeral=True)
+
+        await admin_chat.send(f"""
+@here
+        
+**New role request for {self.dept}**
+
+User: {interaction.user.mention} ({interaction.user.id})
+Name: {self.name.value}
+Department: {self.dept}
+HOD: {self.is_hod}
+
+Run ``/assign user:{interaction.user.id} name:{self.name.value} dept:{self.dept} hod:{self.is_hod}`` to assign this user to the role.
+        """)
+
+        await interaction.response.send_message("Request sent!", ephemeral=True)
 
 class HODView(discord.ui.View):
     def __init__(self, bot: Vivum, user_id: int, dept: str):
@@ -11,12 +52,18 @@ class HODView(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.gray)
-    async def yes(self, button: discord.ui.Button, interaction: discord.Interaction):
-        ...
+    async def yes(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id != self.user_id:
+            return await interaction.response.send_message("You cannot accept this request", ephemeral=True)
     
+        await interaction.response.send_modal(RequestModal(self.bot, self.user_id, self.dept, True))
+
     @discord.ui.button(label="No", style=discord.ButtonStyle.gray)
-    async def no(self, button: discord.ui.Button, interaction: discord.Interaction):
-        ...
+    async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user.id != self.user_id:
+            return await interaction.response.send_message("You cannot accept this request", ephemeral=True)
+
+        await interaction.response.send_modal(RequestModal(self.bot, self.user_id, self.dept, False))
 
 class RequestRolesPersistentView(discord.ui.View):
     def __init__(self, bot: Vivum):
@@ -47,6 +94,11 @@ class RequestRolesPersistentView(discord.ui.View):
         # Check if user is already assigned to a dept
         row = await self.bot.pool.fetchval("SELECT role_name FROM users WHERE user_id = $1", str(interaction.user.id))
 
+        # Ensure user is member
+        if not isinstance(interaction.user, discord.Member):
+            await interaction.response.send_message("You must be a member of the server to request roles", ephemeral=True)
+            return
+
         if row and not (self.bot.config.test_mode and interaction.user.guild_permissions.administrator):
             # Ask if they really want to request a reassignment
             if row == option_selected:
@@ -72,7 +124,7 @@ class RequestRoles(commands.Cog):
     @commands.hybrid_command()
     async def reqroles(self, ctx: commands.Context):
         """Sends the request roles form"""
-        view = RequestRolesPersistentView(ctx)
+        view = RequestRolesPersistentView(ctx.bot)
         msg = await ctx.send(embeds=[
             discord.Embed(
                 title="Request Roles",
